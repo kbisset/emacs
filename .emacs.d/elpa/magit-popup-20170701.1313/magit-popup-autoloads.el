7687792aed977ef01b93bef7f5f5e8c06b223cc1
Keith <keith@light.house>
2017-07-31 21:19:56 -0700

    Update packages

---
+++ .emacs.d/elpa/magit-popup-20170701.1313/magit-popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/magit-popup-20170701.1313/magit-popup-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/magit-popup-20170701.1313/magit-popup-pkg.el"
+;;;;;;  "../../../../.emacs.d/elpa/magit-popup-20170701.1313/magit-popup.el")
+;;;;;;  (22874 47640 717687 55000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
---
+++ .emacs.d/elpa/seq-2.20/seq-autoloads.el
@@ -0,0 +1,18 @@
+;;; seq-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/seq-2.20/seq-24.el"
+;;;;;;  "../../../../.emacs.d/elpa/seq-2.20/seq-25.el" "../../../../.emacs.d/elpa/seq-2.20/seq-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/seq-2.20/seq-pkg.el" "../../../../.emacs.d/elpa/seq-2.20/seq.el")
+;;;;;;  (22874 47641 581674 926000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; seq-autoloads.el ends here
